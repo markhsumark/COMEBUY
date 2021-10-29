bd2b07832aa4498747a9824e8cbe97181c78806e
markhsumark <tulsa0415@gmail.com>
2021-10-29 16:59:41 +0800

    Initial Commit

---
+++ COMEBUY/COMEBUYApp.swift
@@ -0,0 +1,17 @@
+//
+//  COMEBUYApp.swift
+//  COMEBUY
+//
+//  Created by 徐易中 on 2021/10/29.
+//
+
+import SwiftUI
+
+@main
+struct COMEBUYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
